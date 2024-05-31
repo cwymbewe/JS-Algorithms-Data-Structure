d693f8417cc2ad6a691f5a9cf956a3b825880114
cwymbewe <cwymbewe@gmail.com>
2024-05-31 23:34:28 +0200

    "Updated index.html, script.js, and styles.css files with minor changes to links, script tags, and CSS styles."

--- Build a Palindrome Checker Project/script.js
+++ Build a Palindrome Checker Project/script.js
@@ -1,9 +1,11 @@
 const userInput = document.getElementById("text-input");
-
+const eventResult = document.getElementById('check-btn');
 const isEmpty = () => {
     if(userInput.value.length === 0){
         alert("Please input a value");
     }
     return false;
 }
+
+eventResult.addEventListener('click', isEmpty);
 
