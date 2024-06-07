f2e6ab5e05eff29159fb198ca72d902987cce3c4
cwymbewe <cwymbewe@gmail.com>
2024-06-07 20:51:58 +0200

    "Updated link to styles.css to include directory path"

--- Build a Telephone Number Validator Project/main.js
+++ Build a Telephone Number Validator Project/main.js
@@ -6,15 +6,30 @@ const result = document.getElementById('results-div');
 checkBtn.addEventListener('click', () => {
     
     return userInput.value === "" ? alert("Please provide a phone number") : 
-        validateNumber(userInput.value);
+        validate(userInput.value);
 })
 
+const validFormats = [
+    "1 555-555-5555",
+    "1 (555) 555-5555",
+    "1(555)555-5555",
+    "1 555 555 5555",
+    "5555555555",
+    "555-555-5555",
+    "(555)555-5555"
+]
 const regex = /\s?(\d{3}|\(\d{3}\))([\s-])?(\d{3})([\s-])?(\d{4})/
 
-const validateNumber = (str) => {
+const validate = (str) => {
     const reducedStr = str.replace(regex,"");
     
     return result.innerHTML = (reducedStr === "" || reducedStr === "1") ?  `Valid US number: ${userInput.value}` : `Invalid US number: ${userInput.value}`;
 } 
 
 
+console.log(validateNumber(userInput.value));
+
+clearBtn.addEventListener('click', () => {
+    result.innerHTML = "";
+    userInput.value = "";
+})
